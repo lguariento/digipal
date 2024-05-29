14c80ef5107658a5448067141cc02b0cd90d2430
Luca Guariento <lguariento@users.noreply.github.com>
2024-05-29 11:59:57 +0100

    Delete build directory

--- build/__init__.py
+++
@@ -1,3 +0,0 @@
-# What version of Archetype is the latest image on dockerhub
-# See digipal/__init__.py
-__version__ = '2.8'
--- build/build-iipsrv.sh
+++
@@ -1,7 +0,0 @@
-cd /tmp
-git clone https://github.com/ruven/iipsrv.git
-cd iipsrv
-# Deployed in /tmp/iipsrv/src/iipsrv.fcgi
-./autogen.sh && ./configure && make
-mv /tmp/iipsrv/src/iipsrv.fcgi /usr/local/bin/.
-rm -rf /tmp/iipsrv
